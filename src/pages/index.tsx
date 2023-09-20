863f85e89a715e0c731d1515208d224706a0a06b
Federico Minaya <federico.minaya@aerolab.co>
2023-09-20 20:01:37 -0300

    feat: change index page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,47 @@
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const inter = Inter({
+  subsets: ['latin'],
+});
+
 export default function Home() {
   return (
-    <main className="bg-[#212121] text-white h-screen w-screen flex-col gap-10 flex items-center justify-center">
-      <h1 className="text-6xl text-orange-600 font-semibold">Aerolab</h1>
-      <h2 className="text-4xl">Next Starter</h2>
-    </main>
+    <>
+      <Head>
+        <title>Aerolab Next Starter</title>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </Head>
+
+      <main
+        className={`flex min-h-screen flex-col justify-between items-center p-24 ${inter.className}`}
+      >
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Get started by editing&nbsp;
+            <code className="font-mono font-bold">src/pages/index.tsx</code>
+          </p>
+          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <p className="pointer-events-none bg-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0">
+              By Aerolab
+            </p>
+          </div>
+        </div>
+
+        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-orange-700/10 after:dark:from-orange-900 after:dark:via-[#f65100]/40 before:lg:h-[360px]">
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
+
+        <div />
+      </main>
+    </>
   );
 }
