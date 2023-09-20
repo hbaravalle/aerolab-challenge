b899a4e59bdfcc7129baf027b742c3bdc56466e1
Federico Minaya <federico.minaya@aerolab.co>
2023-09-20 18:35:20 -0300

    fix: remove cocos names

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="bg-[#212121] text-white h-screen w-screen flex-col gap-10 flex items-center justify-center">
       <h1 className="text-6xl text-orange-600 font-semibold">Aerolab</h1>
-      <h2 className="text-4xl">🥥 Cocos Capital 🥥</h2>
+      <h2 className="text-4xl">Next Starter</h2>
     </main>
   );
 }
